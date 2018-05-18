7ec51bf94b4f39741017d61f95ee670631697b3f
MarioAlbertoNL1799 <37378931+MarioAlbertoNL1799@users.noreply.github.com>
2018-05-17 23:12:24 -0500

    datos primitivos

---
+++ Programa1.java
@@ -0,0 +1,37 @@
+/**
+ Tic 31
+ matricula 1717110635
+ Datos Primitivos
+ Mario Alberto Nieto Lopez
+ */
+
+package programa1;
+public class Programa1 {
+    public static void main(String[]args) {
+    byte byte_min = -128;
+    byte byte_max = 127;
+    short short_min = -32768;
+    short short_max = 32767;
+    int int_min = -2147483648;
+    int int_max = 2147483647;
+    long long_min = Long.MIN_VALUE;
+    long long_max = Long.MAX_VALUE;
+    float float_min = Float.MIN_VALUE;
+    float float_max = Float.MAX_VALUE;
+    double double_min = Double.MIN_VALUE;
+    double double_max = Double.MAX_VALUE;
+    System.out.println("valor minimo de byte= " + byte_min);
+    System.out.println("valor maximo de byte= " + byte_max);
+    System.out.println("valor minimo de short= " + short_min);
+    System.out.println("valor maximo de short= " + short_max);
+    System.out.println("valor minimo de int= " + int_min);
+    System.out.println("valor maximo de int= " + int_max);
+    System.out.println("valor minimo de long= " + long_min);
+    System.out.println("valor maxim de long= " + long_max);
+    System.out.println("valor minimo de float= " + float_min);
+    System.out.println("valor maximo de float= " + float_max);
+    System.out.println("valor minimo de double= " + double_min);
+    System.out.println("valor maximo de double= " + double_max);
+    }
+
+}
